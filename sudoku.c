1a71a9a595bb2fc90b40e08cfbedaddb0e9811d4
Replit user <>
2023-10-05 06:25:56 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -47,10 +47,34 @@ int is_valid(Node* n){
 
     return 1;
 }
+/*
+1.Cree una función que a partir de un nodo genere una **lista con los nodos adyacentes**:
 
+    List* get_adj_nodes(Node* n){
+       List* list=createList();
+       
+       obtenga los nodos adyacentes a n
+       y agréguelos a la lista
+       
+       return list;
+    }
+
+Recuerde que los nodos adyacentes son generados aplicando las acciones al estado actual.
+
+> - Para el caso del ejemplo, la función debería retornar una lista con **9 nodos**. Cada uno de ellos idéntico al nodo original pero cambiando el valor de la primera casilla vacía, es decir: sudo[0][2], por 1, 2, ..., 9.
+> - Utilice la función Node* copy(Node* n) para copiar nodos.
+*/
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
+
+    for(int i=0;i<9;i++){
+      for(int j=0;i<9;i++)
+    }
+      if(n->sudo[i][j]==0)
+
+
+  
     return list;
 }
 
