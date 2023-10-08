8bf28fb7cc4d999f8a8e5e1f200bace579a02b25
Replit user <>
2023-10-08 22:16:38 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -212,7 +212,39 @@ int is_final(Node* n){
   return 1;  
 }
 
+/*
+5.Implemente la función Node* DFS(Node* n, int* cont). Esta función realiza una búsqueda en profundidad a partir del nodo n. El algoritmo es el siguiente:
+
+1. Cree un stack S (pila) e inserte el nodo.
+2. Mientras el stack S no se encuentre vacío:
+
+   a) Saque y elimine el primer nodo de S.
+   
+   b) Verifique si corresponde a un estado final, si es así retorne el nodo.
+   
+   c) Obtenga la lista de nodos adyacentes al nodo.
+   
+   d) Agregue los nodos de la lista (uno por uno) al stack S.
+   
+   e) Libere la memoria usada por el nodo.
+   
+3. Si terminó de recorre el grafo sin encontrar una solución, retorne NULL.
+
+Almacene en la variable cont, la cantidad de iteraciones que realiza el algoritmo.
+
+*/
+
 Node* DFS(Node* initial, int* cont){
+  Stack * Pila_S = createStack();
+  push(Pila_S, initial);
+  while (is_empty(Pila_S)==0){
+    popFront(Pila_S);
+    
+    
+  }
+  
+  
+  
   return NULL;
 }
 
