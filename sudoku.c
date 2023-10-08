b92368dfb7c3ac6ddd4d7a9987821d0d0a50a198
Replit user <>
2023-10-08 06:30:22 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -142,8 +142,9 @@ List* get_adj_nodes(Node* n){
           Node *copia=copy(n);
           for(int valor=0;valor<9;valor++){     
             if(is_valid(n)){
-              copia=copy(n);
               copia->sudo[i][j]=valor+1;
+              copia=copy(n);
+              //copia->sudo[i][j]=valor+1;
               pushBack(list,copia);
             }
           }
