f656406a44c45554b8d851dd744aa786bf02e7f7
Replit user <>
2023-10-09 00:38:26 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -247,19 +247,21 @@ Node* DFS(Node* initial, int* cont){
     }
 
     List * lista_adyacentes= get_adj_nodes(nodo_sacado);
-    Node* nodo_current= first(lista_adyacentes);
+    Node * nodo_current= first(lista_adyacentes);
+
     
     while(nodo_current!=NULL){
       push(Pila_S,nodo_current);
       nodo_current=next(lista_adyacentes);
     }
-    free(nodo_current);
-
     cont++;
+
+    Node * current2= first(lista_adyacentes);
+    while(current2!=NULL){
+      free(current2);
+      current2=next(lista_adyacentes);
+    }
   }
-  
-  
-  
   return NULL;
 }
 
