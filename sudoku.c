640b2b7f56743c18e55fcc234d3e89c208b535e8
Replit user <>
2023-10-09 00:59:01 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -238,7 +238,7 @@ Node* DFS(Node* initial, int* cont){
   Stack * Pila_S = createStack();
   push(Pila_S, initial);
   while (!is_empty(Pila_S)){
-    (*cont)++;
+    cont++;
     Node *  nodo_sacado = top(Pila_S);
     pop(Pila_S);
     
